c4fc0be34794d9d825be6adcb6a1510f4b483b76
Hambal Abid <43399835+hambal-abid@users.noreply.github.com>
2022-07-31 19:44:40 +0500

    filled the template with dynamic data

--- complete-node-bootcamp-master/1-node-farm/index.js
+++ complete-node-bootcamp-master/1-node-farm/index.js
@@ -4,47 +4,76 @@ const url = require('url');
 
 // ----------------------------FILE--------------------------------
 
-// Blocking, synchronous way
-const textIn = fs.readFileSync('./txt/input.txt','utf-8');
-console.log(textIn);
+// // Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+// console.log(textIn);
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt',textOut);
-console.log('File written!');
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt',textOut);
+// console.log('File written!');
 
 
-// Non-Blocking, asynchronous way
-fs.readFile('./txt/start.txt','utf-8', (err, data1) => {
-    if (err) return console.log('ERROR!');
-    fs.readFile(`./txt/${data1}.txt`,'utf-8', (err, data2) => {
-        console.log(data2);
-        fs.readFile(`./txt/append.txt`,'utf-8', (err, data3) => {
-            console.log(data3);
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-                console.log('File has been written');
-            });
-        });
-    });
-});
-console.log('Will read file!');
+// // Non-Blocking, asynchronous way
+// fs.readFile('./txt/start.txt','utf-8', (err, data1) => {
+//     if (err) return console.log('ERROR!');
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`,'utf-8', (err, data3) => {
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//                 console.log('File has been written');
+//             });
+//         });
+//     });
+// });
+// console.log('Will read file!');
 
 
 // ----------------------------SERVER--------------------------------
 
-//read the file only once starting the program and then return data on each api hit, so synchronus file handling is not a problem
+//the code at top level is executed once on program start and then the data is returned on each api hit, so synchronus file handling is not a problem
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%PRODUCTNAME%}/g, product.organic);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req,res) => {
     const pathName = req.url;
     console.log(pathName);
+
+    //Overview page
     if (pathName === '/' || pathName === '/overview') {
-        res.end(`This is the OVERVIEW`);
+        res.writeHead(200, {'content-type':'text/html'})
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
+
+    //Product page
     } else if (pathName === '/product') {
         res.end(`This is the PRODUCT`);
+
+    //API
     } else if (pathName === '/api') {
         res.writeHead(200, {'content-type' : 'application/json'});
         res.end(data);
+
+    //Not found page
     } else {
         res.writeHead(404, {
             'content-type':'text/html'
